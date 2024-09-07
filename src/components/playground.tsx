9f930652146f865d0cbd73abc34dcbb2f32db939
Ilias Ihadian <i.ihadian@gmail.com>
2024-09-07 17:46:03 +0200

    added responsiveness

--- src/components/playground.tsx
+++ src/components/playground.tsx
@@ -8,8 +8,8 @@ export default function Playground({
   return (
     <main className="p-8 h-screen">
       <div className="border-spacing-8 border-2 border-black rounded-sm h-full  ">
-        <div className="grid grid-flow-row grid-rows-3  gap-4 ">
-          <div className="grid grid-flow-col grid-cols-2 p-8 gap-8">
+        <div className="grid grid-flow-row grid-rows-3  gap-2 ">
+          <div className="grid grid-flow-col grid-cols-2 pt-8 px-8 gap-8">
             <div>
               <h1 className="text-6xl ">REM.</h1>
             </div>
@@ -18,7 +18,7 @@ export default function Playground({
             </div>
           </div>
 
-          <div className="p-8  row-span-2 w-full h-full">{children}</div>
+          <div className="px-8 pb-8  row-span-2 w-full h-auto">{children}</div>
         </div>
       </div>
     </main>
